adc0806cdda2673910483a4266265eb9103da1c8
julian T <julian@jtle.dk>
2019-01-27 21:34:13 +0100

    Removed unused scripts

--- Scripts/mountFstab.sh
+++
@@ -1,7 +0,0 @@
-
-tomnt=$( cat /etc/fstab | grep mnt | sed "s:.* \(/mnt/[^ ]*\) .*:\1:g" | dmenu )
-
-mount $tomnt 
-st -e ranger $tomnt
-sync
-umount $tomnt
--- Scripts/openInRanger.sh
+++
@@ -1,2 +0,0 @@
-#!/bin/bash
-$TERM -e ranger $( echo ~/*/ | sed "s/ /\n/g" | cat .bookmarks - | dmenu -p Dir -l 10 -i | sed "s:~:${HOME}:")
