c09dceb3997ef3e581a442ca9416e886cbeef304
Julian T <julian@jtle.dk>
2019-07-14 21:24:11 +0200

    Replaced initFolders with setupscript system

--- initFolders.sh
+++
@@ -1,8 +0,0 @@
-# Add folders that should not be symlinked by stow
-
-mkdir -p ~/Scripts
-mkdir -p ~/Scripts/enMenuScripts
-mkdir -p ~/.config/qutebrowser
-mkdir -p ~/.config/nvim
-mkdir -p ~/.config/i3
-mkdir -p ~/.config/alot
