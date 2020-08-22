6f4e8b5952dcae7d4f755b73974cdbe1767b7b2c
Julian T <julian@jtle.dk>
2020-08-22 13:01:24 +0200

    New lock screen

---
+++ xinit/Scripts/lock.sh
@@ -0,0 +1,11 @@
+#!/usr/bin/env bash
+
+LOCKFILE=/tmp/lockscreen.png
+
+scrot -o $LOCKFILE
+size=$(identify -format "%[fx:w]x%[fx:h]" "$LOCKFILE")
+
+convert $LOCKFILE -scale 70 -sample $size\! -quality 11 $LOCKFILE
+
+i3lock -i $LOCKFILE
+
